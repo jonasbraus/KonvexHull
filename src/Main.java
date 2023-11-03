8084031a99c69bef523e6fa4b1e3841b3126ea95
brausjonas <brausjonas@gmail.com>
2023-11-03 15:05:03 +0100

    Puuush

--- src/Main.java
+++ src/Main.java
@@ -25,47 +25,64 @@ public class Main
         calculateHull();
     }
 
-    public void calculateHull() {
+    public void calculateHull()
+    {
         List<Point> hull = new ArrayList<>();
-        for (Point p : points) {
-            while (hull.size() >= 2 && orientation(hull.get(hull.size() - 2), hull.get(hull.size() - 1), p) <= 0) {
+
+        for (Point p : points)
+        {
+            while (hull.size() >= 2 && orientation(hull.get(hull.size() - 2), hull.get(hull.size() - 1), p) <= 0)
+            {
                 hull.remove(hull.size() - 1);
             }
             hull.add(p);
         }
+
         int t = hull.size() + 1;
-        for (int i = points.length - 2; i >= 0; i--) {
+        for (int i = points.length - 2; i >= 0; i--)
+        {
             Point p = points[i];
-            while (hull.size() >= t && orientation(hull.get(hull.size() - 2), hull.get(hull.size() - 1), p) <= 0) {
+            while (hull.size() >= t && orientation(hull.get(hull.size() - 2), hull.get(hull.size() - 1), p) <= 0)
+            {
                 hull.remove(hull.size() - 1);
             }
             hull.add(p);
         }
+
+
         hull.remove(hull.size() - 1);
 
-        for(Point p : hull)
+
+
+        for (Point p : hull)
         {
             p.highlight = true;
         }
     }
 
-    private float orientation(Point p, Point q, Point r) {
+    private float orientation(Point p, Point q, Point r)
+    {
         return (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
     }
 
-    public void quickSort(int low, int high) {
-        if (low < high) {
+    public void quickSort(int low, int high)
+    {
+        if (low < high)
+        {
             int pivot = partition(low, high);
             quickSort(low, pivot - 1);
             quickSort(pivot + 1, high);
         }
     }
 
-    private int partition(int low, int high) {
+    private int partition(int low, int high)
+    {
         float pivot = points[high].x;
         int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (points[j].x < pivot) {
+        for (int j = low; j < high; j++)
+        {
+            if (points[j].x < pivot)
+            {
                 i++;
                 Point temp = points[i];
                 points[i] = points[j];
